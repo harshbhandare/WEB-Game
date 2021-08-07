c324affcc96f64249727ae35d8dfdbd8bd390059
harshbhandare <61344060+harshbhandare@users.noreply.github.com>
2021-08-07 10:27:17 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,67 @@
+var buttonColors =["red", "blue", "green", "yellow"];
+
+ var gamePattern=[];
+ var userClickedPattern =[];
+
+ var started = false;
+
+
+var level = 0;
+
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function(){
+  
+  var userChosenColor=$(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+});
+
+
+
+
+
+
+function nextSequence(){
+  level++;
+
+
+  $("#level-title").text("Level " + level);
+
+
+
+    var randomNo=Math.random();
+   var randomNo= Math.floor(randomNo*4);
+   var randomChosenColor=buttonColors[randomNo];
+   gamePattern.push(randomChosenColor);
+   $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+   
+   playSound(randomChosenColor);
+    
+}
+function playSound(name){
+  var audio = new Audio("sounds/" +name+ ".mp3");
+  audio.play();
+}
+function animatePress(currentColor){
+$("#"+currentColor).addClass("pressed");
+
+setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+}, 100);
+}
+
+keyPress(function(){
+  nextSequence();
+})
+
